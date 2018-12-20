565fe10f6daedbdb7f192ea9b9aeb993f1d7f848
Ubuntu <ubuntu@ip-172-31-20-75.us-east-2.compute.internal>
2018-12-20 10:15:37 +0000

    Added validation script

---
+++ validation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys, urllib2, json
+
+contents = urllib2.urlopen("http://localhost:3000/?query=Marina%20Bay%20Sands")
+
+data = json.load(contents)
+
+print("Request status: %s" % data["status"])
+
+if data["status"] != "OK":
+    sys.exit()
+else:
+    sys.exit("Validation result: FAIL")
